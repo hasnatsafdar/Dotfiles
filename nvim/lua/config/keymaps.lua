9cfa1d5b2bf43a835a643446ce93bcee6a4802ae
Hasnat Safdar <hasnatprofessional@gmail.com>
2025-04-18 16:52:39 +0500

    my Fedora + SwayWM configuration files
    Some of these are my own but most of them are the files i either got from the official repos or by other people and then i tweaked them to my prefrences. The reason to keep them here is to keep them organized. Feel free to copy any of them you like.

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,16 @@
+local map = vim.keymap.set
+local unmap = vim.api.nvim_del_keymap
+
+map('n', ';', ':', { noremap = true, silent = false })
+map('n', '<leader>e', ':Neotree filesystem reveal left<CR>', {})
+map('n', '.', ':lua if vim.fn.exists(":NeoTree") == 2 then vim.cmd("Neotree toggle hidden") end<CR>', { noremap = true, silent = true })
+
+map('i', '<C-h>', '<Left>', { noremap = true, silent = true })
+map('i', '<C-j>', '<Down>', { noremap = true, silent = true })
+map('i', '<C-k>', '<Up>', { noremap = true, silent = true })
+map('i', '<C-l>', '<Right>', { noremap = true, silent = true })
+
+map("n", "<S-h>", "<C-w>h", { desc = "Focus Left" })
+map("n", "<S-j>", "<C-w>j", { desc = "Focus Down" })
+map("n", "<S-k>", "<C-w>k", { desc = "Focus Up" })
+map("n", "<S-l>", "<C-w>l", { desc = "Focus Right" })
